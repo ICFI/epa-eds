a1b34881d36114bbee706e6ce78a6623c103c2cd
Patrick Tisdel <patricktisdel@outlook.com>
2015-12-17 14:18:51 -0500

    Moving files to suit my organization

---
+++ public/scripts/angularScripts.js
@@ -0,0 +1,23 @@
+
+var app = angular.module("EPAApp", ['ngSanitize'] );
+
+
+
+
+
+app.controller("AppController", function ($scope, $window, $rootScope, $compile, $timeout) {
+
+    
+
+});
+
+
+
+
+
+
+
+
+
+
+
