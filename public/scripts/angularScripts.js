27ad67e2a279c9265c02ac8ce507bde88a3ef219
Patrick Tisdel <patricktisdel@outlook.com>
2015-12-22 13:57:25 -0500

    Made accessibility and styling fixes. Made dropdowns more noticeable.

--- public/scripts/angularScripts.js
+++ public/scripts/angularScripts.js
@@ -8,11 +8,9 @@ app.controller("AppController", function ($window, $scope) {
     
     vm.gallonsFactor = 112.523911;
     vm.carsFactor = 0.210526;
-    vm.coalFactor = 0.000000263;
+    vm.coalFactor = 0.000000263;    
     
-    
-    vm.sidebarOpen=true;
-      
+    vm.sidebarOpen=true;      
     
     vm.loadStateData = function () {
         $.getJSON('data/stateData.json')
@@ -21,22 +19,30 @@ app.controller("AppController", function ($window, $scope) {
             vm.states = data;
             $scope.$apply();
         });
-    };
+    };    
     
-    
-    
-    
-    vm.selectedState=null;
-    
-    vm.selectedPage="takingAction";
-    
+    vm.selectedState=null;    
+    vm.selectedPage="takingAction";    
     vm.loadStateData();
-    
-   
 
 });
 
-
+app.filter('writeOutBillions', function() {
+  return function(number) {
+    
+    var n = number.replace(/[^\d\.\-]/g, "") || ''; 
+    n = parseFloat(n);
+    var out = "";
+      
+    if (n>999999999) {
+        out=(n/1000000000).toFixed(2)+" billion";
+    }
+    else {
+        out=number;
+    }
+    return out;
+  };
+})
 
 
 
